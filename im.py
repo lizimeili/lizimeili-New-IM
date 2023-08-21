be7bee06505dcd795d3149898a69eb606ae17fd0
lizimeili <122461767+lizimeili@users.noreply.github.com>
2023-08-21 18:51:30 +0800

    IM 2.0
    我是不是更新太快了，增加了游戏功能。然后就是你想要做其他语言版可以邮箱联系邮箱是：lizimeili@outlook.com，不要把l打成i了

--- im.py
+++ im.py
@@ -1,5 +1,5 @@
 from random import randint
-
+import time
 
 
 print("i        m         m")
@@ -12,7 +12,7 @@ print("i        m   m m   m")
 print("by:lizimeili")
 
 while True:
-    nemu = input("文本写入请输1,随机数请输2,退出请输3")
+    nemu = input("文本写入请输1,随机数请输2,游戏请输3,退出请输4")
     nemu = int(nemu)
     if nemu == 1:
         data = input("写入内容")
@@ -28,4 +28,31 @@ while True:
         ne = randint(yok1,yok2)
         print(ne)
     elif nemu ==3:
-        break
+        print("游戏")
+        print("狗还是猫,1")
+        print("随机钓鱼,2")
+        print("????")
+        game = input()
+        game = int(game)
+        
+        if game == 1:
+            amlit = randint(1,2)
+            if amlit == 1:
+                aml = "狗"
+            if amlit == 2:
+                aml = "猫"
+            amlinpt = input("狗还是猫?")
+            if amlinpt == aml:
+                print("答对")
+            else:
+               print("不对")
+        if game == 2:
+            input("按回车丢鱼饵")
+            print("等待三秒")
+            time.sleep(3)   
+            ne = randint(1,100)   
+            if ne <=50:
+                print("鱼跑走啦")
+            if ne >51 and ne == 100:
+                print("钓鱼成功")  
+            
